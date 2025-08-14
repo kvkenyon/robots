55eca2ae9259a28850166b861da76191ed8731b1
Kevin <kvkenyon@gmail.com>
2025-08-14 13:15:18 -0500

    add se2

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,20 @@ fn main() {
     println!("{}", rot);
     println!("det(R)={}", rot.det());
     println!("det(RxR)={}", (rot * rot).det());
+
+    let degrees: f64 = 30.0;
+
+    let trot = motion::trot2(degrees.to_radians());
+
+    println!("{}", trot);
+
+    let transl = motion::transl2(1.0, 2.0);
+
+    println!("{}", transl);
+
+    let res = transl * trot;
+    println!("{}", res);
+
+    let t1 = motion::se2(1.0, 2.0, 30.0f64.to_radians());
+    println!("{}", t1);
 }
--- src/motion.rs
+++ src/motion.rs
@@ -5,3 +5,23 @@ pub fn rot2(theta: f64) -> linalg::Matrix<2, 2> {
         data: [[theta.cos(), -theta.sin()], [theta.sin(), theta.cos()]],
     }
 }
+
+pub fn trot2(theta: f64) -> linalg::Matrix<3, 3> {
+    linalg::Matrix {
+        data: [
+            [theta.cos(), -theta.sin(), 0.0],
+            [theta.sin(), theta.cos(), 0.0],
+            [0.0, 0.0, 1.0],
+        ],
+    }
+}
+
+pub fn transl2(x: f64, y: f64) -> linalg::Matrix<3, 3> {
+    linalg::Matrix {
+        data: [[1.0, 0.0, x], [0.0, 1.0, y], [0.0, 0.0, 1.0]],
+    }
+}
+
+pub fn se2(tx: f64, ty: f64, theta: f64) -> linalg::Matrix<3, 3> {
+    transl2(tx, ty) * trot2(theta)
+}
